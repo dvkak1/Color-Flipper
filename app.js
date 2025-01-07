c1a4ada22a21f7923184bd97124de3fabb2883d5
dvkak1 <dv_kakilala@yahoo.com>
2025-01-07 15:11:19 +0800

    see previous commit msg

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f1f5025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function() {
-   //Generate random number between 0 - 3  colors[3] 
+   //Generate random number between 0 - 3  colors[0] 
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
-    
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 })
 
 function getRandomNumber() {
-    return Math.random();
+    //The math random function generates a number between
+    //0 and 0.9 so it cannot generate a number that is an integer
+    //so to fix this. Math.floor function is used multiplied with 
+    //the product between Math.random() and colors.length
+    return Math.floor(Math.random() * colors.length);
 }
